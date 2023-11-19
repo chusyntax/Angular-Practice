b2d1e5409af84f804b7299bb5d4ede71458044d7
Chu <ttheko101@gmail.com>
2023-11-19 17:31:40 +0200

    Lessons 92-103

--- prac-3/src/app/app.module.ts
+++ prac-3/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
+import { UnlessDirective } from './unless.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    BasicHighlightDirective,
+    BetterHighlightDirective,
+    UnlessDirective
   ],
   imports: [
     BrowserModule,
@@ -17,3 +23,4 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+// Now let's use this directive. To use it, we have to do two things. First of all, like for a component, we have to inform Angular that we have a new directive. Just like with components, it doesn't scan all our files, so it doesn't know. So we have to go to app module, and here in declarations, we have to add our BasicHighlightDirective and also add the import pointing to the basic highlight folder and there to the BasicHighlightDirective file.
